576d9c6f862f3cecd64d88b6695652fa39a70b60
Minh Nguyen <mnguyenn@protonmail.com>
2022-05-11 16:18:41 -0700

    * Necessary role update. * Improve readability in main.go and README.

--- Readme.md
+++ Readme.md
@@ -8,19 +8,26 @@
   $ az aks create --resource-group demo --name memberCluster --node-count 1  --generate-ssh-keys --enable-managed-identity
 ```
 
-- Create namespace in the hub cluster, aka `member-a`.
+- Create a namespace for test purpose in hub cluster
+
+```sh
+ $ kubectl apply -f namespace.yaml
+```
+
 - Create Role and RoleBinding
 
 ```sh
  $ kubectl apply -f hub_roles.yaml
 ```
 
-- Get the member cluster vmss managed identity
+- Get the member cluster vmss managed identity (*preq:* Install `jq`. An easy option is to install with `homebrew`)
   
   ```sh
   $ az aks show --resource-group demo --name memberCluster | jq .identityProfile.kubeletidentity.clientId
   ```
+  Update var `MemberClusterClientId` in `main.go` with this value.
 
+- Get `hubCluster` server api address (an easy option is using Azure portal) and update var `HubServerApiAddress` in `main.go` with this value.
 - Create new managed identity Credential using the member cluster clientId
 - Use AKS scope `6dae42f8-4368-4678-94ff-3960e28e3630` to create TokenRequestOptions.
 - Get the token from the created managed identity.
@@ -44,3 +51,7 @@
  $ kubectl apply -f member-agent-deployment.yaml
 ```
 
+- **Expectations:** 
+  - The log should show that access to pods is forbidden for namespace `default'.
+  - The log should show that there is 0 pod in `member-a` namespace and no error.
+  - Try deploy `pod.yaml` in hub cluster. After successful deployment. The log should show that pod `demo-msi` is found in namespace `member-a`
--- main.go
+++ main.go
@@ -15,27 +15,34 @@ import (
 	"k8s.io/client-go/rest"
 )
 
-var MEMBER_CLUSTER_CLIENT_ID = "<MEMBER_CLUSTER_CLIENT_ID>"
+var (
+	HubServerApiAddress   = ""
+	MemberClusterClientId = ""
+	Namespace             = "member-a"
+	Pod                   = "demo-msi"
+)
+
+const AKSScope = "6dae42f8-4368-4678-94ff-3960e28e3630"
 
 func main() {
 
-	clientID := azidentity.ClientID(MEMBER_CLUSTER_CLIENT_ID)
+	clientID := azidentity.ClientID(MemberClusterClientId)
 	opts := &azidentity.ManagedIdentityCredentialOptions{ID: clientID}
 	managed, err := azidentity.NewManagedIdentityCredential(opts)
 	if err != nil {
-		fmt.Printf("error creating the managed identity. err: %v", err.Error())
+		fmt.Printf("\nerror creating the managed identity. err: %v", err.Error())
 	}
 
 	token, err := managed.GetToken(context.TODO(), policy.TokenRequestOptions{
-		Scopes: []string{"6dae42f8-4368-4678-94ff-3960e28e3630"},
+		Scopes: []string{AKSScope},
 	})
 	if err != nil {
-		fmt.Printf("error getting the token. err: %v", err.Error())
+		fmt.Printf("\nerror getting the token. err: %v", err.Error())
 	}
 
 	cf := rest.Config{
 		BearerToken: token.Token,
-		Host:        "https://hubcluster-demo-043560-9a369bac.hcp.westus.azmk8s.io:443",
+		Host:        HubServerApiAddress,
 		TLSClientConfig: rest.TLSClientConfig{
 			Insecure: true,
 		},
@@ -44,29 +51,36 @@ func main() {
 	// create the clientset
 	clientset, err := kubernetes.NewForConfig(&cf)
 	if err != nil {
-		fmt.Printf("error creating clientset. err: %v", err.Error())
+		fmt.Printf("\nerror creating clientset. err: %v", err.Error())
 	}
+
 	for {
+		fmt.Printf("start pod listing loop")
+		// this should fail as access wasn't granted for namespace 'default'
 		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
 		if err != nil {
-			fmt.Printf("error listing pods. err: %v", err.Error())
+			fmt.Printf("\nerror listing pods. err: %v\n", err.Error())
 		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
-		namespace := "default"
-		pod := "demo-msi"
-		_, err = clientset.CoreV1().Pods(namespace).Get(context.TODO(), pod, metav1.GetOptions{})
+		// this should succeed
+		pods, err = clientset.CoreV1().Pods(Namespace).List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			fmt.Printf("\nerror listing pods. err: %v", err.Error())
+		}
+		fmt.Printf("\nThere are %d pods in the cluster\n", len(pods.Items))
+
+		_, err = clientset.CoreV1().Pods(Namespace).Get(context.TODO(), Pod, metav1.GetOptions{})
 		if errors.IsNotFound(err) {
-			fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
+			fmt.Printf("\npod %s in namespace %s not found\n", Pod, Namespace)
 		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %s in namespace %s: %v\n",
-				pod, namespace, statusError.ErrStatus.Message)
+			fmt.Printf("\nerror getting pod %s in namespace %s: %v\n",
+				Pod, Namespace, statusError.ErrStatus.Message)
 		} else if err != nil {
 			panic(err.Error())
 		} else {
-			fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
+			fmt.Printf("\nfound pod %s in namespace %s\n", Pod, Namespace)
 		}
-		fmt.Print("Nothing so far....")
+		fmt.Printf("end pod listing loop")
 		time.Sleep(1 * time.Second)
 	}
 }
